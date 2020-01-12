cd6b78e2760087217fa819027768820c0403c6b5
Samuel Rowan <samuel@samuelrowan.com>
2020-01-11 19:17:32 -0600

    It doesn't work but I'm getting there.

---
+++ starwars/src/components/characterList.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./characterCard";
+
+export default function CharacterList() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.co/api/people/`)
+            .then(response => {
+                setCharacters(response.results);
+            })
+            .catch(error => {
+                console.log("sucks to suck", error);
+            })
+    })
+    console.log(characters);
+
+    return (
+        <div className="character">
+            {characters.map(character => {
+                return (
+                    <CharacterCard 
+                        key={character.id}
+                        name={character.name}
+                        species={character.species}
+                    />
+                )
+            })}
+        </div>
+    );
+}
